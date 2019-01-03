c6d7863d2232dcbbc558942a2e4cadf16856bbe6
William Oliveira de Lagos <william.lagos@icloud.com>
2019-01-03 18:16:22 -0200

    Removed mail form and replaced with MailChimp integration

--- index.php
+++ index.php
@@ -200,29 +200,32 @@ if(isset($_POST['submit'])){
     </div>
   </div>
   <div class="container">
-    <div class="row">
+    <div class="row text-center">
       <div class="col-md-12">
         <br><br>
-        <h1 class="text-center">Gostou? Entre em contato com a gente.</h1><br>
-        <form action="" method="post">
-          <div class="form-group">
-            <label for="firstname">Nome</label>
-            <input class="form-control" id="firstname" type="text" name="first_name">
-          </div>
-          <div class="form-group">
-            <label for="lastname">Sobrenome</label>
-            <input class="form-control" id="lastname" type="text" name="last_name">
-          </div>
-          <div class="form-group">
-            <label for="email">E-mail</label>
-            <input class="form-control" id="email" type="email" name="e_mail">
-          </div>
-          <div class="form-group">
-            <label for="message">Mensagem</label>
-            <textarea class="form-control" id="message" rows="5" name="message" cols="30"></textarea>
-          </div>
-          <input class="btn btn-primary btn-block" type="submit" name="submit" value="Enviar">
-        </form>
+        <h1 class="text-center">Gostou? Saiba mais se inscrevendo em nossa newsletter.</h1>
+        <!-- Begin Mailchimp Signup Form -->
+        <div id="mc_embed_signup">
+          <form action="https://efforia.us15.list-manage.com/subscribe/post?u=05c870f4c948a5f5b75f65c4c&amp;id=58cc28237a" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+            <div id="mc_embed_signup_scroll">
+              <div class="mc-field-group input-group input-group-lg px-sm-5 my-3">
+                <!-- <label for="mce-EMAIL">Email Address </label> -->
+                <input type="email" value="" name="EMAIL" class="required email form-control" id="mce-EMAIL">
+              </div>
+          	  <div id="mce-responses" class="clear">
+          		  <div class="response" id="mce-error-response" style="display:none"></div>
+          		  <div class="response" id="mce-success-response" style="display:none"></div>
+          	  </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+              <div style="position: absolute; left: -5000px;" aria-hidden="true">
+                <input type="text" name="b_05c870f4c948a5f5b75f65c4c_58cc28237a" tabindex="-1" value="">
+              </div>
+              <div class="clear pt-3">
+                <input class="btn btn-outline-dark btn-lg" type="submit" value="Inscrever" name="subscribe" id="mc-embedded-subscribe">
+              </div>
+            </div>
+          </form>
+        </div>
+        <!--End mc_embed_signup-->
         <br><br>
       </div>
     </div>
